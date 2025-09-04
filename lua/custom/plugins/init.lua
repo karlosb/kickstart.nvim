31934a600d71c983dde74711110b14792ab6c543
Karl Bowers <karlbowers2@googlemail.com>
2025-09-04 22:49:54 +0100

    custom plugins ~ install: nvim-tree devicons

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -23,3 +23,11 @@
     end,
   },
   {
+    'nvim-tree/nvim-web-devicons',
+    lazy = false,
+    enabled = true,
+    config = function()
+      require('nvim-web-devicons').setup {}
+    end,
+  },
+  {
